85b409faf7ae23455da24e0b428228d896f04fea
Willian Sant' Anna <w.s.santanna@gmail.com>
2025-08-11 20:57:03 -0300

    chore: Simplificação da configuração do Swagger
    - Remoção da funcionalidade "tryOpenInDefaultBrowser";
    - Remoção de algumas configurações globais da dependência.

--- modules/api/src/main/java/br/com/dbserver/api/config/SwaggerConfig.java
+++ modules/api/src/main/java/br/com/dbserver/api/config/SwaggerConfig.java
@@ -3,13 +3,8 @@ package br.com.dbserver.api.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
-
-import java.awt.Desktop;
-import java.net.URI;
 
 @Configuration
 public class SwaggerConfig {
@@ -28,17 +23,4 @@ public class SwaggerConfig {
             );
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void tryOpenInDefaultBrowser() {
-        try {
-            System.out.println("Abrindo interface Swagger UI no seu navegador padrão...");
-            
-            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-                Desktop.getDesktop().browse(new URI("http://localhost:8080/swagger-ui.html"));
-            }
-            System.out.println("Disponível no endereço http://localhost:8080/swagger-ui.html");
-        } catch (Exception e) {
-            System.err.println("Não foi possível abrir o navegador automaticamente. Por favor, acesse o endereço http://localhost:8080/swagger-ui.html");
-        }
-    }
 }
